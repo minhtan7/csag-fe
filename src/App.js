f94a65c71dc667580fa46f94a5c53dfb74533928
minhtan7 <74660901+minhtan7@users.noreply.github.com>
2021-08-10 11:54:38 +0700

    Revert "Hue"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Hompage from "./pages/Hompage";
 import ShipperPage from "./pages/ShipperPage";
