32067d34648ce0bfa4244b4a611596f5f27d2d12
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-08-10 09:35:03 +0700

    merge Van's work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Hompage from "./pages/Hompage";
+import Hompage from "./pages/Homepage/Hompage";
 import ShipperPage from "./pages/ShipperPage";
+import PublicNav from "./components/PublicNav/PublicNav";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 function App() {
   return (
     <Router>
+      <PublicNav />
       <Switch>
         <Route exact path="/" component={Hompage} />
         <Route path="/shipper" component={ShipperPage} />
