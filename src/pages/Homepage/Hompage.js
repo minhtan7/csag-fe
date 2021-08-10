855012b751061e96759dc46d7295713a58300fa9
Your Name <you@example.com>
2021-08-10 19:08:29 +0700

    marker

--- src/pages/Homepage/Hompage.js
+++ src/pages/Homepage/Hompage.js
@@ -1,22 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
 
-import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
-import MainForm from '../../components/Form/MainForm';
-import Map from '../../components/Map/Map';
+import MainForm from "../../components/Form/MainForm";
+import Map from "../../components/Map/Map";
 import { useSelector, useDispatch } from "react-redux";
-import { formActions } from '../../redux/actions/form.action';
+import { formActions } from "../../redux/actions/form.action";
 import { userActions } from "../../redux/actions/user.actions";
-import './style.css';
+import "./style.css";
 
 const Hompage = () => {
-	const dispatch = useDispatch();
-	const [show, setShow] = useState(false);
-	const handleClose = () => {
-		setShow(false);
-		dispatch(formActions.changePage('index'));
-	};
-	const handleShow = () => setShow(true);
+  const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    setShow(false);
+    dispatch(formActions.changePage("index"));
+  };
+  const handleShow = () => setShow(true);
   const loading = useSelector((state) => state.user.loading);
   const users = useSelector((state) => state.user.users);
   let pageNum;
@@ -25,26 +24,31 @@ const Hompage = () => {
     dispatch(userActions.getAllUsers({ pageNum, limit }));
   }, []);
 
-	return (
-		<div id="map-container">
+  return (
+    <div id="map-container">
       <Map users={users} />
-			<Modal show={show} onHide={handleClose} fullscreen="sm-down" contentClassName="form-modal-content" scrollable>
-				{/* <Modal.Header closeButton>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        fullscreen="sm-down"
+        contentClassName="form-modal-content"
+        scrollable
+      >
+        {/* <Modal.Header closeButton>
 					<Modal.Title>Modal heading</Modal.Title>
 				</Modal.Header> */}
-				<Modal.Body className="d-flex justify-content-center p-0 main-form-modal">
-					<MainForm handleClose={handleClose} />
-				</Modal.Body>
-				{/* <Modal.Footer></Modal.Footer> */}
-			</Modal>
-			<div className="join-button">
-				<Button variant="dark" size="lg" onClick={handleShow}>
-					Tôi muốn tham gia
-				</Button>
-			</div>
-		</div>
-	);
-
+        <Modal.Body className="d-flex justify-content-center p-0 main-form-modal">
+          <MainForm handleClose={handleClose} />
+        </Modal.Body>
+        {/* <Modal.Footer></Modal.Footer> */}
+      </Modal>
+      <div className="join-button">
+        <Button variant="dark" size="lg" onClick={handleShow}>
+          Tôi muốn tham gia
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Hompage;
