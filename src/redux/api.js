36ad9fc88f9c3ca07e4dee829cdd3d6ffd2dff9b
Hue203 <huenguyenmt.vn@gmail.com>
2021-08-10 19:03:14 +0700

    shipperCard edit

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_API + "/api",
+  baseURL: process.env.REACT_APP_URL + "/api",
   headers: {
     "Content-Type": "application/json",
     authorization: "Bearer " + localStorage.getItem("accessToken"),
