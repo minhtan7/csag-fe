c76af478ef6278fec5bc70a23eeaaf119ffdcee7
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2021-08-11 06:34:28 +0700

    ??

--- src/pages/Homepage/Hompage.js
+++ src/pages/Homepage/Hompage.js
@@ -12,7 +12,6 @@ import MarkerPopup from '../../components/Map/MarkerPopup';
 import { mapActions } from '../../redux/actions/map.actions';
 
 const Hompage = () => {
-<<<<<<< HEAD
 	const user = useSelector((state) => state.auth.user);
 	const dispatch = useDispatch();
 	const [show, setShow] = useState(false);
@@ -20,22 +19,6 @@ const Hompage = () => {
 		setShow(false);
 		dispatch(formActions.changePage('index'));
 	};
-=======
-  const dispatch = useDispatch();
-  const [show, setShow] = useState(false);
-  const handleClose = () => {
-    setShow(false);
-    dispatch(formActions.changePage("index"));
-  };
-  const handleShow = () => setShow(true);
-
-  const users = useSelector((state) => state.user.users);
-  let pageNum;
-  let limit = 2000;
-  useEffect(() => {
-    dispatch(userActions.getAllUsers({ pageNum, limit }));
-  }, []);
->>>>>>> e3ce03a4f014512e9d6aaf23bd7db94c3c1b66b9
 
 	const handleShow = () => setShow(true);
 	const loading = useSelector((state) => state.user.loading);
